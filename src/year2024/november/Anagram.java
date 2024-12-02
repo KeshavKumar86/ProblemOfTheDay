df6e591033fa0c9abf579fddc0d92717eaafbc05
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-02 16:30:26 +0530

    Day-27 Anagram Time Complexity: O(n + m)Auxiliary Space: O(1) Improved Solution

--- src/year2024/november/Anagram.java
+++ src/year2024/november/Anagram.java
@@ -11,13 +11,12 @@ public class Anagram {
 
         //This will take constant space
         int[] charCount = new int[26];
+        if (s1.length() != s2.length())
+            return false;
         //This will take linear O(n) time
-        for (char c : s1.toCharArray()) {
-            charCount[c - 'a']++;
-        }
-        //This will take linear o(m) time
-        for (char c : s2.toCharArray()) {
-            charCount[c - 'a']--;
+        for (int i = 0; i < s1.length(); i++) {
+            charCount[s1.charAt(i) - 'a']++;
+            charCount[s2.charAt(i) - 'a']--;
         }
         //This will take constant O(1) time
         for (int i : charCount) {
@@ -27,3 +26,30 @@ public class Anagram {
         return true;
     }
 }
+/*
+Brute Force: A brute force solution for checking if two strings are anagrams involves comparing all
+possible arrangements of one string with the other string. This approach, while not efficient,
+guarantees correctness.
+
+Here’s how a brute force solution might look:
+
+Generate All Permutations:
+Generate all permutations of one string (e.g., s1) and check if any of them equals the other string
+(s2).
+Time Complexity:
+Generating permutations of a string of length 𝑛 has a factorial time complexity:
+O(n!).
+Space Complexity:
+
+Storing all permutations in a Set takes
+O(n!⋅n) space in the worst case, where n is the string length.
+
+2nd Solution: Sort both the String and check if they are equal or not.
+Time Complexity: O(nlogn)
+Space Complexity: O(1)
+
+Optimal Solution: increment the frequency of each character for s1 and decrease the frequency for s2
+finally check if frequency array contains all Zeros.
+Time Complexity: O(n)
+Space Complexity: O(1)
+ */
