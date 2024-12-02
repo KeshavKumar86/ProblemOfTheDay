c82bc8ce2a688b5c7acc556ede947385260c36bd
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-02 15:39:18 +0530

    Day-27 NonRepeatingCharacter

---
+++ src/year2024/december/NonRepeatingCharacter.java
@@ -0,0 +1,32 @@
+package year2024.december;
+
+public class NonRepeatingCharacter {
+
+    public static void main(String[] args) {
+        String s = "aabbcc";
+        System.out.println("Result: " + nonRepeatingChar(s));
+
+    }
+    static char nonRepeatingChar(String s) {
+
+        int[] arr = new int[26];
+        //below is a linear time operation
+        for (char c : s.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        //below is a linear time operation
+        for (char c : s.toCharArray()) {
+            if (arr[c - 'a'] == 1) {
+                return c;
+            }
+        }
+        return '$';
+    }
+
+/*
+Brute-Force Solution: using 2 loops checks if one character repeats(O(n)), and do it for every
+character(O(n)) present in the String
+Optimal Solution:  The array-based approach is the most optimal in both time and space for this
+specific problem (limited to lowercase Latin letters)
+ */
+}
