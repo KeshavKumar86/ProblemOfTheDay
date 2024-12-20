6eff59df2d95f2bfc9abc66ebcc6a619727894f7
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-20 10:03:18 +0530

    Day-44 AllocateMinimumPages Time Complexity: O((log(sum-max)*n) Space complexity: O(1)

--- src/year2024/december/AllocateMinimumPages.java
+++ src/year2024/december/AllocateMinimumPages.java
@@ -3,8 +3,8 @@ package year2024.december;
 public class AllocateMinimumPages {
     public static void main(String[] args) {
 
-        int[] arr = {15, 10, 19, 10, 5, 18, 7};
-        int k = 5;
+        int[] arr = {22, 23, 67};
+        int k = 1;
         System.out.println("Result: " + findPages(arr, k));
 
     }
@@ -15,13 +15,23 @@ public class AllocateMinimumPages {
         int high = sum(arr);
         if (k > arr.length)
             return -1;
-        for (int pages = low; pages <= high; pages++) {
+        /*for (int pages = low; pages <= high; pages++) {
             int count = minStudent(arr, pages);
             if (count <= k) {
                 return pages;
             }
+        }*/
+        //Binary search
+        while (low <= high) {
+            int pages = (low + high) / 2;
+            int minStudent = minStudent(arr, pages);
+            if (minStudent > k) {
+                low = pages + 1;
+            } else {
+                high = pages - 1;
+            }
         }
-        return -1;
+        return high + 1;
     }
 
     private static int minStudent(int[] arr, int maxPages) {
@@ -56,3 +66,33 @@ public class AllocateMinimumPages {
         return max;
     }
 }
+/*
+Solution1: Naive Solution: using linear search
+Intuition: we have to minimize the maximum number of pages we can allocate to a student given the
+condition that: every student should get at least one book.
+every book should be allocated
+To minimize the number of pages, assign the books to the maximum student(initially equal to no of
+books).
+Now we cannot assign the books to more than k students. so we have to decrease the student.
+To decrease the students, we have to increase the number of pages, and we have to keep the number of
+pages as minimum as possible, given the condition that we cannot assign the books to more than k
+students.
+So we will start with the minimum number of pages possible and check if we can meet the condition that
+number of students<k, if we cannot, we will increase the no of pages by 1.
+we keep doing it until we reach either the no of student<k or
+maximum no of pages possible.
+Best Case: when we have no of students equal to number of books.In this case minimum no of pages
+possible equal to max(arr)
+Worst Case: when we have 1 student, so he will get all books, so minimum number of pages possible
+equal to sum(arr).
+To find the number of students that will be allocated books for a page limit, we start assigning
+books to the first student until the page limit is reached, then we move to the next student and
+so on. As soon as we find the first page limit with which we can allocate books to all k students,
+we will return it.
+Time complexity: O((sum-max)*n)
+Space complexity: O(1)
+
+Solution2: optimal Solution: using binary search
+Time complexity: O(log(sum-max)*n)
+Space complexity: O(1)
+ */
