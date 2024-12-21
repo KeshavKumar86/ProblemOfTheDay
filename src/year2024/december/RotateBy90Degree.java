7b557130ce97f7e82446cd4049b0b8f8385244f8
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-21 19:51:29 +0530

    Day-45 RotateBy90Degree Time Complexity: O((n*n) Space complexity: O(1)

--- src/year2024/december/RotateBy90Degree.java
+++ src/year2024/december/RotateBy90Degree.java
@@ -5,31 +5,40 @@ import java.util.Arrays;
 public class RotateBy90Degree {
     public static void main(String[] args) {
 
-        int[][] arr = new int[3][];
-        arr[0] = new int[]{1, 2, 3};
-        arr[1] = new int[]{4, 5, 6};
-        arr[2] = new int[]{7, 8, 9};
+        int[][] arr = new int[2][];
+        arr[0] = new int[]{1, 2};
+        arr[1] = new int[]{3, 4};
         for (int[] element : arr) {
             System.out.println(Arrays.toString(element));
         }
-        rotateby90(arr);
+        rotateBy90(arr);
         System.out.println("After Rotation:");
         for (int[] element : arr) {
             System.out.println(Arrays.toString(element));
         }
     }
 
-    static void rotateby90(int[][] mat) {
+    static void rotateBy90(int[][] mat) {
 
         int n = mat.length;
-        int[][] res = new int[n][n];
+        //first transpose the matrix
         for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j >= 0; j--) {
-                res[n - 1 - j][i] = mat[i][j];
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
-        for (int i = 0; i < n; i++) {
-            System.arraycopy(res[i], 0, mat[i], 0, n);
+        //reverse every column
+        for (int col = 0; col < n; col++) {
+            int low = 0, high = n - 1;
+            while (low <= high) {
+                int temp = mat[low][col];
+                mat[low][col] = mat[high][col];
+                mat[high][col] = temp;
+                low++;
+                high--;
+            }
         }
     }
 }
