90fe18c1d09c4f72191ce906b7c2741afe664c74
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-24 09:56:06 +0530

    Day-48 SearchInASortedMatrix Time Complexity: O(logn+logm) Space complexity: O(1)

--- src/year2024/december/SearchInASortedMatrix.java
+++ src/year2024/december/SearchInASortedMatrix.java
@@ -8,7 +8,7 @@ public class SearchInASortedMatrix {
         arr[0] = new int[]{1, 5, 9};
         arr[1] = new int[]{14, 20, 21};
         arr[2] = new int[]{30, 34, 43};
-        int x = 15;
+        int x = 20;
 
         System.out.println("Search Result: " + searchMatrix(arr, x));
 
@@ -16,25 +16,51 @@ public class SearchInASortedMatrix {
 
     private static boolean searchMatrix(int[][] mat, int x) {
 
-        //linear search
+        //binary search
         int n = mat.length;
         int m = mat[0].length;
-        int i = 0, j = m - 1;
-        while (i < n && j >= 0) {
-            if (mat[i][j] == x) {
+        //first check in which row element will be present
+        int low = 0, high = n - 1;
+        int col = m - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mat[mid][col] == x) {
                 return true;
-            } else if (mat[i][j] > x) {
-                //move left
-                j--;
+            } else if (mat[mid][col] > x) {
+                high = mid - 1;
             } else {
-                i++;
+                low = mid + 1;
+            }
+        }
+        //in above loop either you will find element or you will get the row that should have that
+        //element
+        int row = low;
+        if (row >= n) {
+            return false;
+        }
+        low = 0;
+        high = m - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mat[row][mid] == x) {
+                return true;
+            } else if (mat[row][mid] > x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return false;
     }
 }
 /*
-Solution1: Naive Solution
+Solution1: Naive Approach Brute force Approach
+Iterate every element and compare it with target, if found return true else return false
+Time complexity: O(n*m)
+Space complexity: O(1)
+
+Solution2: Better Solution
 start from the top-right corner
 check if x is less than the element,
   if x is less move left
@@ -42,6 +68,22 @@ check if x is less than the element,
 Time complexity: O(n+m)
 Space complexity: O(1)
 
-Solution2: optimal Solution
+Solution3: More Better Solution (Using binary search twice)
+take last column and low = 0 and high = n-1 and find the row that should have element.
+then use binary search to search that element in that particular row.
+Time complexity: O(logn + logm)
+logn - To find the row that should have that element
+logm - To find the element in that particular row.
+Space complexity: O(1)
+
+Solution4: Optimal Search (using binary search once)
+Take low = 0 and high = n*m-1
+find middle and apply below formula to find the row and column
+row = middle/no of column;
+column = middle%no of column;
+Apply binary search logic.
+Time complexity: O(logn*m)
+Space complexity: O(1)
+
 
  */
