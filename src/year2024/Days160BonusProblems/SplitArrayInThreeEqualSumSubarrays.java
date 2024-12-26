9d882f0bc16c3e6e0beed82c77b75e78e832d6ec
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-26 11:35:21 +0530

    Day-50 SplitArrayInThreeEqualSumSubarrays Time complexity: O(n) Space complexity: O(1)

--- src/year2024/Days160BonusProblems/SplitArrayInThreeEqualSumSubarrays.java
+++ src/year2024/Days160BonusProblems/SplitArrayInThreeEqualSumSubarrays.java
@@ -8,43 +8,51 @@ public class SplitArrayInThreeEqualSumSubarrays {
     public static void main(String[] args) {
 
         //arr[] = [1, 3, 4, 0, 4]
-        int[] arr = {1, 3, 4, 0, 4};
+        int[] arr = {3, 2, 4};
         List<Integer> result = findSplit(arr);
         System.out.println("Result: " + result);
     }
 
     private static List<Integer> findSplit(int[] arr) {
-        // Return an array of possible answer, driver code will judge and return true or
-        // false based on
-        List<Integer> list = new ArrayList<>();
 
+        List<Integer> list = new ArrayList<>();
         int sum = Arrays.stream(arr).sum();
+        list.add(-1);
+        list.add(-1);
         if (sum % 3 != 0) {
-            list.add(-1);
-            list.add(-1);
             return list;
         }
+
         int targetSum = sum / 3;
         int commulativeSum = 0;
-        boolean possible = false;
         for (int i = 0; i < arr.length; i++) {
             if ((commulativeSum + arr[i]) < targetSum) {
                 commulativeSum += arr[i];
             } else if ((commulativeSum + arr[i]) == targetSum) {
-                list.add(i);
-                if (list.size() == 2) {
-                    possible = true;
-                    break;
+                if (list.get(0) == -1) {
+                    list.set(0, i);
+                } else {
+                    list.set(1, i);
+                    return list;
                 }
                 commulativeSum = 0;
             }
         }
-        if (possible) {
-            return list;
+
+        if (list.get(1) == -1) {
+            list.set(0, -1);
         }
-        list.clear();
-        list.add(-1);
-        list.add(-1);
         return list;
     }
 }
+/*
+Solution1: Optimal Solution
+Find the total sum and check if it is divisible by 3, if it is than solution is possible otherwise
+return -1,-1.
+If it is divisible by 3 than we have to find 3 consecutive sub-arrays which has sum = totalSum/3;
+Start from index 0 and calculate cumulative sum if it is equal to target we got our first subarray.
+Add this index to result, and make cumulative sum zero again to find the second subarray.
+Repeat the same process and when we find the second subarray break the loop or return the result.
+Time Complexity: O(n)
+Space complexity : O(1)
+ */
