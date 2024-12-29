3f2f692dffdd3f23407a07870042ed6e08d4e175
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-29 10:02:46 +0530

    Day-53 FindAllTripletsWithZeroSum Time complexity: O(n^m) Space complexity: O(n^3)

--- src/year2024/december/FindAllTripletsWithZeroSum.java
+++ src/year2024/december/FindAllTripletsWithZeroSum.java
@@ -16,24 +16,41 @@ public class FindAllTripletsWithZeroSum {
 
     private static List<List<Integer>> findTriplets(int[] arr) {
 
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
         List<Integer> list;
         int n = arr.length;
 
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> value;
+            if (map.containsKey(arr[i])) {
+                value = map.get(arr[i]);
+            } else {
+                value = new ArrayList<>();
+            }
+            value.add(i);
+            map.put(arr[i], value);
+        }
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((arr[i] + arr[j] + arr[k]) == 0) {
+                int target = -(arr[i] + arr[j]);
+                if (map.containsKey(target)) {
+                    List<Integer> value = map.get(target);
+                    for (int element : value) {
                         list = new ArrayList<>();
-                        list.add(i);
-                        list.add(j);
-                        list.add(k);
-                        result.add(list);
+                        if (i != element && j != element) {
+                            list.add(i);
+                            list.add(j);
+                            list.add(element);
+                            Collections.sort(list);
+                            result.add(list);
+                        }
+
                     }
                 }
             }
         }
-        return result;
+        return new ArrayList<>(result);
     }
 }
 /*
@@ -41,5 +58,19 @@ Solution1: Brute Force Solution
 Check all possible triplets and check which are the triplet's sum equal to zero, and add them into
 the result;
 Time complexity: O(n*n*n)
-Space complexity: O(1)
+Space complexity: In the worst case, where every combination of three elements forms a valid triplet,
+the number of unique triplets can be at most n(n−1)(n−2)/6, which simplifies to O(n^3) for space.
+
+Solution2: Improved Solution
+Iterate through the array and store the values as keys in map and indices in the list as values
+of map.
+Iterate using 2 loops and search the -(sum) in the map and if it is present, iterate through the all
+indices of that values and add to the resulted set.
+Worst Case: O(n^3), when the map's lists contain a large number of indices for many pairs.
+Average Case: Closer to O(n^2)
+for most real-world inputs, assuming the map's lists are relatively short.
+Space complexity: O(n^3) (no. of triplets possible in worst case)
+
+Solution3: Optimal solution
+
  */
