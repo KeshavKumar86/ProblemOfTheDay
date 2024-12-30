c1e36418776580d1139172778b7be489f939c8e0
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-30 09:04:12 +0530

    Day-54 UnionOfArraysWithDuplicates Time complexity: O(n+m) Space complexity: O(n+m)

--- src/year2024/december/UnionOfArraysWithDuplicates.java
+++ src/year2024/december/UnionOfArraysWithDuplicates.java
@@ -35,16 +35,16 @@ Time Complexity: O((n+m)*(n+m))
 Space complexity: O(1)
 
 Solution2: By Using Sorting
-Intuition: Sort both arrays.
-Merge the two arrays while counting distinct elements.
+Intuition: 1.Sort both arrays.
+2.Merge the two arrays while counting distinct elements.
 Sorting: O(nlogn+mlogm)
 Merging: O(n+m)
 Overall Time complexity: O(nlogn+mlogm)
 Space complexity: O(1)
 
 Solution3: optimal Solution
-Add the elements of array a into the set
-Add the elements of array b into the set
+1.Add the elements of array a into the set
+2.Add the elements of array b into the set
 return the size of the set.
 Time Complexity: O(n+m) on an avg.
 Space complexity: O(n+m)
