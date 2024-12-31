b800af190a9adaa8398dcfca8954b044e4cf1a6e
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-31 10:17:12 +0530

    Day-55 LongestConsecutiveSubsequence Time complexity: O(n) Space complexity: O(n)

--- src/year2024/december/LongestConsecutiveSubsequence.java
+++ src/year2024/december/LongestConsecutiveSubsequence.java
@@ -1,33 +1,45 @@
 package year2024.december;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestConsecutiveSubsequence {
     public static void main(String[] args) {
 
         //arr[] = [2, 6, 1, 9, 4, 5, 3]
-        int[] arr = new int[]{0, 1, 1, 1, 2, 3, 4, 5, 7, 8};
+        int[] arr = new int[]{99, 80, 60, 33, 44, 86, 34, 64, 77, 18, 39, 3, 27,
+                16, 29, 60, 67, 23, 7, 87, 6, 75, 92, 11, 59, 13, 88, 24, 98, 4, 26, 6};
         int result = longestConsecutive(arr);
         System.out.println("Result: " + result);
     }
 
     private static int longestConsecutive(int[] arr) {
 
-        Arrays.sort(arr);
-        int n = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
         int maxLength = 1;
-        int length = 1;
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] + 1 == arr[i + 1]) {
-                length++;
-            } else if (arr[i] == arr[i + 1]) {
-                continue;
+        for (int j : arr) {
+            if (map.containsKey(j - 1)) {
+                map.put(j, map.get(j - 1) + 1);
+                maxLength = Math.max(maxLength, map.get(j - 1) + 1);
+                int key = j;
+                while (map.containsKey(key + 1)) {
+                    map.put((key + 1), map.get(key) + 1);
+                    maxLength = Math.max(maxLength, map.get(key) + 1);
+                    key++;
+                }
+            } else if (map.containsKey(j + 1)) {
+                map.put(j, 1);
+                int key = j;
+                while (map.containsKey(key + 1)) {
+                    map.put(key + 1, map.get(key) + 1);
+                    maxLength = Math.max(maxLength, map.get(key) + 1);
+                    key++;
+                }
             } else {
-                maxLength = Math.max(maxLength, length);
-                length = 1;
+                map.put(j, 1);
             }
         }
-        return Math.max(maxLength, length);
+        return maxLength;
     }
 }
 /*
@@ -38,5 +50,11 @@ has max length than the known maximum.
 Time complexity: O(nlogn)
 Space complexity: O(1)
 
-Solution2: Optimal Solution
+Solution2: Optimal Solution using 1 iteration
+Intuition: Use a hashmap to store the number and the consecutive sequence till this number,
+if map.contains(arr[i] -1) than for arr[i] sequence length will be length(arr[i]-1)+1, and we have to
+also update the sequence length for numbers arr[i]+ if they are present.
+if(map.contains(arr[i] +1 ) than we have to update the length of all sequence by 1 for values arr[i]++
+Time Complexity: O(n)
+Space complexity: O(n)
 */
