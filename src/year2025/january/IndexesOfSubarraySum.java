4aad1e3343e0732af5b5018b2e28507a7dccb891
Keshav Kumar <keshavgujj99@gmail.com>
2025-01-13 17:51:05 +0530

    Day-68 IndexesOfSubarraySum Time Complexity: O(n) Space Complexity: O(1)

--- src/year2025/january/IndexesOfSubarraySum.java
+++ src/year2025/january/IndexesOfSubarraySum.java
@@ -7,10 +7,10 @@ import java.util.Map;
 public class IndexesOfSubarraySum {
     public static void main(String[] args) {
 
-        //arr[] = [1, 2, 3, 7, 5], target = 12
+        //arr[] = [1, 2, 3, 7, 5], target = 15
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int target = 15;
-        System.out.println("Result: " + subarraySum(arr, target));
+        System.out.println("Result: " + subarraySumOptimal(arr, target));
     }
 
     private static ArrayList<Integer> subarraySum(int[] arr, int target) {
@@ -34,15 +34,46 @@ public class IndexesOfSubarraySum {
         list.add(-1);
         return list;
     }
+
+    private static ArrayList<Integer> subarraySumOptimal(int[] arr, int target) {
+
+        int i = 0, j = 0;
+        int sum = 0;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (j < arr.length) {
+            sum += arr[j];
+                while (i<=j && sum > target) {
+                    sum -= arr[i];
+                    i++;
+                }
+            if(sum == target) {
+                arrayList.add(i + 1);
+                arrayList.add(j + 1);
+                return arrayList;
+            }
+            j++;
+        }
+        arrayList.add(-1);
+        return arrayList;
+    }
 }
 /*
 Solution1: Brute Force Solution
 Iterate every subarray using 2 loops and check if the sum is equal to target, if it is, add the indexes
-into the result list and return, if not found then add -1 amd return.
+into the result list and return, if not found then add -1 and return.
 Time Complexity: O(n^2)
 Space Complexity: O(1)
 
 Solution2: Optimal Solution (Using sliding window)
+Intuition: As we have to find the subarray sum (subarray and sum indicate that 2 pointers can be used)
+Because there is no window size, and we have to find the size of the subarray, so we should use variable
+size sliding window.
+Take 2 pointers i and j.
+if (sum<target)
+move j.
+else if(sum>target)
+move i until sum>target.
+else return the result.
 Time Complexity: O(n)
 Space Complexity: O(1)
 
