bc244c58a80e6091ad389f71fc8e492a926fb73d
Keshav Kumar <keshavgujj99@gmail.com>
2025-01-16 08:17:34 +0530

    Day-71 LargestSubarrayOf0And1 Time Complexity: O(n) Space Complexity: O(n)

---
+++ src/year2025/january/LargestSubarrayOf0And1.java
@@ -0,0 +1,50 @@
+package year2025.january;
+
+import java.util.HashMap;
+
+public class LargestSubarrayOf0And1 {
+    public static void main(String[] args) {
+
+        //arr[] = [1, 0, 1, 1, 1, 0, 0]
+        int[] arr = new int[]{0, 0, 1, 1, 0};
+        System.out.println("Result: " + maxLen(arr));
+    }
+
+    private static int maxLen(int[] arr) {
+        // Your code here
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                arr[i] = -1;
+            }
+        }
+        int maxLength = 0;
+        int prefixSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < n; i++) {
+            prefixSum += arr[i];
+            if (map.containsKey(prefixSum)) {
+                maxLength = Math.max(maxLength, i - map.get(prefixSum));
+            } else {
+                map.put(prefixSum, i);
+            }
+        }
+        return maxLength;
+    }
+}
+/*
+Solution1: Brute Force Solution
+Generate every subarray using 2 loops and count no of 1's, if the size == 2*no of 1's then take the
+length and compare with max.
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+
+Solution2: Optimal Solution
+Replace every 0's with -1. Now question is boiled down to find the longest subarray with 0 sum.
+We can find that using Hashmap and prefixSum. check if prefixSum is already present, if it is already
+present that count the length, and do not put this prefixSum(it will reduce the length). Compare
+the length with maxLength.
+Time Complexity: O(n)
+Space Complexity: O(n)
+ */
