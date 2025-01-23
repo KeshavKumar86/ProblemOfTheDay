7adb3442ca51405fccf7b1ecd71511621c7bf536
Keshav Kumar <keshavgujj99@gmail.com>
2025-01-23 20:00:10 +0530

    Day-77 CloneListWithNextAndRandom Time Complexity: O(2n) Space Complexity: O(2n)

--- src/year2025/january/CloneListWithNextAndRandom.java
+++ src/year2025/january/CloneListWithNextAndRandom.java
@@ -10,6 +10,11 @@ public class CloneListWithNextAndRandom {
         head1.next.next = new Node(10);
         head1.next.next.next = new Node(13);
         head1.next.next.next.next = new Node(15);
+        Node result = cloneLinkedList(head1);
+        while(result!=null){
+            System.out.print(result.data + " -> ");
+            result = result.next;
+        }
     }
 
     private static Node cloneLinkedList(Node head) {
