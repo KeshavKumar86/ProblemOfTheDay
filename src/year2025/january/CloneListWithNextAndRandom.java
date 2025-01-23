82e2565c2bab7e86e721ad09a67357a4f8242673
Keshav Kumar <keshavgujj99@gmail.com>
2025-01-23 19:58:11 +0530

    Day-77 CloneListWithNextAndRandom Time Complexity: O(2n) Space Complexity: O(2n)

---
+++ src/year2025/january/CloneListWithNextAndRandom.java
@@ -0,0 +1,33 @@
+package year2025.january;
+
+import java.util.HashMap;
+
+public class CloneListWithNextAndRandom {
+    public static void main(String[] args) {
+
+        Node head1 = new Node(5);
+        head1.next = new Node(7);
+        head1.next.next = new Node(10);
+        head1.next.next.next = new Node(13);
+        head1.next.next.next.next = new Node(15);
+    }
+
+    private static Node cloneLinkedList(Node head) {
+
+        HashMap<Node, Node> map = new HashMap<>();
+        if (head == null) {
+            return null;
+        }
+        Node temp = head;
+        while (temp != null) {
+            map.put(temp, new Node(temp.data));
+            temp = temp.next;
+        }
+        temp = head;
+        while (temp != null) {
+            map.get(temp).next = map.get(temp.next);
+            temp = temp.next;
+        }
+        return map.get(head);
+    }
+}
